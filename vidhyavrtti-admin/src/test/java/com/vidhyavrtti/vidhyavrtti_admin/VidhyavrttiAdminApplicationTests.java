a4bc2932a3a7295b41022885b378613a455ed4aa
kanhaiya RD <118708023+Kana121@users.noreply.github.com>
2025-01-18 18:41:26 +0000

    initial project setup

---
+++ vidhyavrtti-admin/src/test/java/com/vidhyavrtti/vidhyavrtti_admin/VidhyavrttiAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhyavrtti.vidhyavrtti_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidhyavrttiAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
