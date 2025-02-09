0afe003af01a69bc4c73b81a9963ce6c19d3ab6a
kanhaiya RD <118708023+Kana121@users.noreply.github.com>
2025-02-08 19:41:35 -0800

    added admin server
    added admin server to maintain all endpoint actuator from single place

---
+++ admin-server/src/test/java/com/krd/in/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krd.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
